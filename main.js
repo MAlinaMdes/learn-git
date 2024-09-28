e6215eda4bba8db26ef0737f7a1eb49fad5f3b5b
M_Alina design <amamina@ru.ndl.norbert-dentressangle.com>
2024-09-28 03:47:36 +0300

    initial git commit -m initial q git status git commit -m initial

---
+++ main.js
@@ -0,0 +1,9 @@
+const nameOfGood = 'апельсин';
+const quantity = 20;
+const typeOfGood = 'froot';
+const price = 100;
+
+console.log('nameOfGood: ', nameOfGood);
+
+let totalPrice = quantity * price;
+console.log('totalPrice: ', totalPrice);
